8acf21a3448031c147d635cc0655ec66d32f6012
akashjsr63 <126679302+akashjsr63@users.noreply.github.com>
2024-07-10 14:18:15 +0530

    Create guest.js

---
+++ src/middlewares/guest.js
@@ -0,0 +1,17 @@
+
+const guest = async (req, res, next) => {
+    try {
+        let query = req.query;
+        if (query.user != process.env.GUEST_ID) {
+            return res.status(400).json({ message: 'Invalid guest ID' });
+        }
+        
+        next();
+    } catch (err) {
+        res.status(401).json({ message: 'Something went wrong', error: err });
+    }
+}
+
+module.exports = {
+    guest
+}

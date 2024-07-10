e335d49d0498419e1d9f4f8138a232b264f218f1
akashjsr63 <126679302+akashjsr63@users.noreply.github.com>
2024-07-10 14:23:45 +0530

    Update guest.js

--- src/middlewares/guest.js
+++ src/middlewares/guest.js
@@ -2,7 +2,7 @@
 const guest = async (req, res, next) => {
     try {
         let query = req.query;
-        if (query.user != process.env.GUEST_ID) {
+        if (!query.user || query.user != process.env.GUEST_ID) {
             return res.status(400).json({ message: 'Invalid guest ID' });
         }
         
